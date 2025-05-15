5488ad0e01da714f6333917cad113fb57f654345
Valentin Ferreira <ferreira.pro37@gmail.com>
2025-05-15 15:08:08 +0200

    versel

--- src/components/trainerCard/trainerCardButton/TrainerCardButton.tsx
+++ src/components/trainerCard/trainerCardButton/TrainerCardButton.tsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
+interface ToggleType {
+    onToggleTrainerCard: () => void;
+}
 
-function TrainerCardButton({ onToggleTrainerCard }) {
-	const [activeImage, setActiveImage] = useState(false);
-	const handleClick = () => {
-		setActiveImage(true);
-	};
-	return (
-		<img
-			src="src/assets/images/trainerCard/trainer_card.svg"
-			alt="trainer card"
-			onClick={onToggleTrainerCard}
-			onKeyDown={(e) => e.key === "Enter" && handleClick()}
-		/>
-	);
+function TrainerCardButton({ onToggleTrainerCard }: ToggleType) {
+    return (
+        <img
+            src="src/assets/images/trainerCard/trainer_card.svg"
+            alt="trainer card"
+            onClick={onToggleTrainerCard}
+            onKeyDown={(e) => e.key === "Enter" && onToggleTrainerCard()}
+        />
+    );
 }
 
 export default TrainerCardButton;
